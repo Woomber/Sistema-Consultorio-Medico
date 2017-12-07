e1adef632140b848285d1daee0d2bc9a8082b000
commi <nosremhc@gmail.com>
2017-12-07 08:12:22 -0600

    paciente

--- Consultorio GUI/FormPaciente.cs
+++ Consultorio GUI/FormPaciente.cs
@@ -13,7 +13,11 @@ namespace Consultorio_GUI
 {
     public partial class FormPaciente : Form
     {
-        WebServiceSoapClient client;
+        WebService1SoapClient client;
+
+        List<Paciente> uno;
+        List<Cita> dos;
+        List<Horario> horarios;
 
         public int CuentaActual { get; set; }
         //de por mientras
@@ -23,104 +27,102 @@ namespace Consultorio_GUI
         {
             InitializeComponent();
 
-            client = new WebServiceSoapClient();
+            client = new WebService1SoapClient();
 
             CuentaActual = actual;
 
             actualizarDatos();
+
+           uno = client.readPaciente().Where(y=>y.ID_Cuenta==CuentaActual).ToList();
+
+            horarios = client.readHorario().ToList();
+            
+            listNuevaCita_Hora.DisplayMember = "hora";
         }
-        
+
         void actualizarDatos()
         {
-            WebService.WebServiceSoapClient x = new WebServiceSoapClient();
-          //  List<modeloPaciente> pa = x.readPaciente().ToList();
+            txtInfoPaciente_Nombres.Text = uno[0].nombre;
+            txtInfoPaciente_ApellidoPaterno.Text = uno[0].apellidoPaterno;
+            txtInfoPaciente_ApellidoMaterno.Text = uno[0].apellidoMaterno;
+            txtInfoPaciente_TipoSangre.Text = uno[0].tipoSangre;
+            txtInfoPaciente_Sexo.Text = uno[0].sexo;
+            txtInfoPaciente_Fecha.Text = Convert.ToString(uno[0].fechaNacimiento);
 
-           /*xtInfoPaciente_Nombres.DataSource = pa;
-            txtInfoPaciente_ApellidoPaterno.Text = pa.;
-            txtInfoPaciente_ApellidoMaterno.Text = na.apellidoMaterno;
-            txtInfoPaciente_TipoSangre.Text = na.tipoSangre;
-            txtInfoPaciente_Sexo.Text = na.sexo;
- /          txtInfoPaciente_Fecha.Text = na.fechaNacimiento;
-
-           int idmedico = pa.idMedico;
-            List<modeloCita> res = x.readCita(idmedico,pacienteactual);
-            dgvVerCita.DataSource = res;
+            List<Cita> dos = client.readCita().ToList();
+            var d = from u in dos
+                    where u.ID_Paciente == uno[0].ID
+                    select u;
+            
+            dgvVerCita.DataSource = d.ToList();
             //Actualizar aquí todo lo automático
-            //Mostrar información del paciente*/
+            //Mostrar información del paciente
         }
 
         private void btnInfoPaciente_Guardar_Click(object sender, EventArgs e)
         {
-            WebService.WebServiceSoapClient x = new WebServiceSoapClient();
+            WebService.WebService1SoapClient x = new WebService1SoapClient();
 
-            string nom = txtInfoPaciente_Nombres.Text;
-            string apep = txtInfoPaciente_ApellidoPaterno.Text;
-            string apem = txtInfoPaciente_ApellidoMaterno.Text;
-            string sang = txtInfoPaciente_TipoSangre.Text;
-            string sex = txtInfoPaciente_Sexo.Text;
-         //   DateTime fech = new DateTime(txtInfoPaciente_Fecha.);//. o partir
             string tel = txtInfoPaciente_Telefono.Text;
             string dire = txtInfoPaciente_Direccion.Text;
 
-
-            //bool res = x.updatePaciente(pacienteactual, nom, apep, apem, sang, fech, sex, tel, dire);//que es username?
-/*            if (res == true)
+           bool res = client.updatePaciente(uno[0].ID, uno[0].ID_Cuenta, uno[0].nombre, uno[0].apellidoMaterno, uno[0].apellidoPaterno, uno[0].tipoSangre, uno[0].fechaNacimiento, uno[0].sexo,tel,dire, uno[0].ID_Medico);
+           if (res == true)
             {
                 MessageBox.Show("Datos modificados");
             }
             else
             {
                 MessageBox.Show("Error al cambiar datos");
-            }*/
+            }
         }
 
         private void btnInfoPaciente_Historial_Click(object sender, EventArgs e)
         {
             //Reemplazar 0 por paciente actual
-            FormHistorial form = new FormHistorial(0);
+            FormHistorial form = new FormHistorial(uno[0].ID);
             form.ShowDialog();
         }
 
         private void btnInfoPaciente_Vacuna_Click(object sender, EventArgs e)
         {
             //Reemplazar 0 por paciente actual, dejar el false
-            FormVacunas form = new FormVacunas(0, false);
+            FormVacunas form = new FormVacunas(uno[0].ID, false);
             form.ShowDialog();
         }
 
         private void btnNuevaCita_Agendar_Click(object sender, EventArgs e)
         {
-            WebService.WebServiceSoapClient x = new WebServiceSoapClient();//ir?
 
             string mo = txtNuevaCita_Motivo.Text;
-            int hor = listNuevaCita_Hora.SelectedIndex;//no se me ocurrio otra cosa
+            Horario hor = listNuevaCita_Hora.SelectedItem as Horario;
             DateTime fecha = calendarNuevaCita.SelectionStart;
-            bool res = x.createCita(hor, 1, pacienteactual, true, mo, fecha);
-        }
 
-        private void btnInfoPaciente_Confirmar_Click(object sender, EventArgs e)
-        {
-            WebService.WebServiceSoapClient x = new WebServiceSoapClient();
-            string contra = txtInfoPaciente_Password.Text;
-            string userna = txtInfoPaciente_Nombres.Text;
-            bool res = x.updateCuenta(3, contra, userna, "Paciente");//de donde el id
-            if (res == true)
-            {
-                MessageBox.Show("Contraseña modificada");
-            }
-            else
-            {
-                MessageBox.Show("Error al modificar contraseña");
-            }
-        }//ya?
+            dos = client.readCita().Where(y => y.ID_Paciente == uno[0].ID).ToList();
+            int res = client.createCita(fecha, mo, true, uno[0].ID, uno[0].ID_Medico, hor.ID);
+        }
 
         private void cbRecetas_Receta_SelectedIndexChanged(object sender, EventArgs e)
         {
-            WebService.WebServiceSoapClient x = new WebServiceSoapClient();
+            WebService.WebService1SoapClient x = new WebService1SoapClient();
 
-            modeloReceta selec = cbRecetas_Receta.SelectedItem as modeloReceta;
+            Receta selec = cbRecetas_Receta.SelectedItem as Receta;
+            dgvRecetas_Medicamentos.DataSource = x.readMedicamentoReceta();
+        }
 
-            List<modeloReceta> yu = x.readReceta();
+        private void calendarNuevaCita_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            Dictionary<DayOfWeek, string> fechas = new Dictionary<DayOfWeek, string>();
+
+            fechas.Add(DayOfWeek.Monday, "L");
+            fechas.Add(DayOfWeek.Tuesday, "M");
+            fechas.Add(DayOfWeek.Wednesday, "X");
+            fechas.Add(DayOfWeek.Thursday, "J");
+            fechas.Add(DayOfWeek.Friday, "V");
+            fechas.Add(DayOfWeek.Saturday, "S");
+            fechas.Add(DayOfWeek.Sunday, "D");
+
+            listNuevaCita_Hora.DataSource = horarios.Where(x => x.ID_Medico == uno[0].ID_Medico && x.dia == fechas[calendarNuevaCita.SelectionStart.DayOfWeek]).ToList();
         }
     }
 }
